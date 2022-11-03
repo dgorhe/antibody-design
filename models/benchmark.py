ffb5913cca72cdd8f26361fb790ba4f873dfbf9c
Darvesh Gorhe <dgorhe123@gmail.com>
2022-11-03 15:47:20 -0400

    Created function to calculate n-gram dictionary for arbitrary lengths n

--- models/benchmark.py
+++ models/benchmark.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.special import kl_div
+from collections import defaultdict
+from tqdm import tqdm
 
 def generate_random_subsets(x, sample_size, samples=10):
     subsamples = []
@@ -10,7 +12,8 @@ def generate_random_subsets(x, sample_size, samples=10):
 
     return subsamples
 
-def find_kl_divergence(x, y):
+def subsampled_kl_divergence(x, y):
+
     x_subsets = generate_random_subsets(x, sample_size=10, samples=10)
     y_subsets = generate_random_subsets(y, sample_size=10, samples=10)
     try:
@@ -29,16 +32,37 @@ def find_kl_divergence(x, y):
 
     return cumulative_kl
 
-def find_n_gram_distribution(examples):
-    pass
+def calculate_n_gram(examples: list[str], n=2) -> dict:
+    """
+    Calculates number of occurances of n characters in a string
+    by moving a sliding window of size n across each string.
+
+    Example:
+        AAABCDC should return a dictionary as follows:
+        {'AA': 2, 'AB': 1, 'BC': 1, 'CD': 1, 'DC': 1}
+    """
+    n_gram = defaultdict(lambda: 0)
+    
+    for ex in tqdm(examples):
+        for idx in range(len(ex) - n):
+            key = ex[idx:idx+n]
+            n_gram[key] += 1
+
+    return n_gram
+
 
 if __name__ == "__main__":
-    x = np.random.normal(2, 1, 20); x = x[x > 0]
-    y = np.random.normal(2, 1, 20); y = y[y > 0]
+    # # Testing subsampled_k_divergence()
+    # x = np.random.normal(2, 1, 20); x = x[x > 0]
+    # y = np.random.normal(2, 1, 20); y = y[y > 0]
 
-    print(x)
-    print(y)
+    # print(x)
+    # print(y)
+    # print(subsampled_kl_divergence(x, y))
 
-    print(find_kl_divergence(x, y))
+    # Testing calculate_n_gram
+    test_examples = ["fhjdsljaf", "rueiworuwe", "fjkdsljfkl;sdj", "hfdlsabv,xcb"]
+    dist = calculate_n_gram(test_examples, n=3)
+    print(dist)
 
     
