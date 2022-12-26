4fd3320a2e2736446fbb01aca0d7dd077da7f634
Darvesh Gorhe <dgorhe123@gmail.com>
2022-12-25 19:08:11 -0500

    Added argument parsing and removed inference

--- models/vae/train.py
+++ models/vae/train.py
@@ -1,73 +1,54 @@
-import torch 
-import torchvision.datasets as datasets
 from tqdm import tqdm
+from scipy.io import mmread
+import torch
 from torch import optim, nn
 from model import VariationalAutoEncoder
-from torchvision import transforms
-from torchvision.utils import save_image
-from torch.utils.data import DataLoader
+from argparse import ArgumentParser
 
+# Argument Parsing
+parser = ArgumentParser(
+    description = "Variational AutoEncoder training script",
+    epilog = "See https://github.com/dgorhe/antibody-design for usage and instructions"
+)
+
+parser.add_argument("-d", "--data", default="../../data/encoded_antibodies.mtx", help='File path for training data. Should be mtx file of dimension (training examples, example size)')
+parser.add_argument("-hdim", default=200, help='Number of nodes in hidden dimension')
+parser.add_argument("-zdim", default=20, help='Number of for latent space mu and sigma')
+parser.add_argument("-n", default=20, help='Number of epochs to train for')
+parser.add_argument("-batchsize", default=64, help='Number of training data points per batch')
+args = parser.parse_args()
+
+# Matrix of dimension (training data points, data point length)
+data = mmread(args.data)
+data = torch.tensor(data, dtype=torch.float32)
+
+# Settings for training
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-INPUT_DIM = 784
-H_DIM = 200
-Z_DIM = 20
-NUM_EPOCHS = 10
-BATCH_SIZE = 64
-# LR_RATE = 3e-4 # Karpathy constant
-LR_RATE = 1e-3 # Karpathy constant
+INPUT_DIM = data.shape[0]
+H_DIM = args.hdim
+Z_DIM = args.zdim
+NUM_EPOCHS = args.n
+BATCH_SIZE = args.batchsize
+LR_RATE = 1e-3
 
-# Dataset Loading
-dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
-train_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
+# Instantiate model
 model = VariationalAutoEncoder(INPUT_DIM, H_DIM, Z_DIM).to(DEVICE)
 optimizer = optim.Adam(model.parameters(), lr=LR_RATE)
 loss_fn = nn.BCELoss(reduction='sum')
 
 for epoch in range(NUM_EPOCHS):
-    loop = tqdm(enumerate(train_loader))
-
-    for i, (x, _) in loop:
+    for x in tqdm(data, total=data.shape[0], desc=f'Epoch {epoch}'):
         # Forward pass
-        x = x.to(DEVICE).view(x.shape[0], INPUT_DIM)
+        x.to(DEVICE)
         x_reconstructed, mu, sigma = model(x)
-
-        # Compute loss
+        
         reconstruction_loss = loss_fn(x_reconstructed, x)
+        
+        # KL Divergence = 1 + log(sigma^2) - mu^2 - sigma^2
         kl_div = -torch.sum(1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2))
-
-        # Backprop
+        
+        # Backward pass
         loss = reconstruction_loss + kl_div
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        loop.set_postfix(loss=loss.item())
-
-model.to("cpu")
-
-def inference(digit, num_examples=1):
-    images = []
-    idx = 0
-    for x, y in dataset:
-        if y == idx:
-            images.append(x)
-            idx += 1
-        if idx == 10:
-            break
-
-    encodings_digit = []
-    for d in range(10):
-        with torch.no_grad():
-            mu, sigma = model.encode(images[d].view(1, 784))
-        encodings_digit.append((mu, sigma))
-
-    mu, sigma = encodings_digit[digit]
-    for example in range(num_examples):
-        epsilon = torch.randn_like(sigma)
-        z = mu + sigma + epsilon
-        out = model.decode(z).view(-1, 1, 28, 28)
-        save_image(out, f"generated_{digit}_ex{example}.png")
-
-
-for idx in range(10):
-    inference(idx, num_examples=1)
-
